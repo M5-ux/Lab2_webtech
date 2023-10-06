7729e625706a1c280a89a3561ac0cc059776c367
Jonathan VELIN <jonathan.velin@edu.ece.fr>
2023-10-06 11:38:55 +0200

    App

---
+++ FrontEnd /my-app/app/page.js
@@ -0,0 +1,11 @@
+import Image from 'next/image'
+import styles from './page.module.css'
+
+export default function Home() {
+  return (
+    <div>
+      <h1>Bienvenue sur ma page d'accueil Next.js !</h1>
+      <p>C'est ma première page avec Next.js.</p>
+    </div>
+  );
+}
