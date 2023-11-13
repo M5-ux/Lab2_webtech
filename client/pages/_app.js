6ea3b7523065d4e8af7a8e00ca53e3effa89f578
M5-ux <mathy97280@gmail.com>
2023-11-13 08:47:47 +0100

    lab8

--- client/pages/_app.js
+++ client/pages/_app.js
@@ -1,12 +1,20 @@
 import 'styles/globals.css';
-import { UserProvider } from './components/UserContext';
+import { createPagesBrowserClient } from '@supabase/auth-helpers-nextjs'
+import { SessionContextProvider } from '@supabase/auth-helpers-react'
+import { useState } from 'react'
 
 function MyApp({ Component, pageProps }) {
+
+  const [supabaseClient] = useState(() => createPagesBrowserClient())
+
   return (
-    <UserProvider>
+    <SessionContextProvider
+      supabaseClient={supabaseClient}
+      initialSession={pageProps.initialSession}
+    >
       <Component {...pageProps} />
-    </UserProvider>
-  );
+    </SessionContextProvider>
+  )
 }
 
 export default MyApp;
---
+++ client/pages/admin/contacts/[id]/index.js
@@ -0,0 +1,54 @@
+//export default async function Post() {
+// const { data } = await supabase.from('contact').select()
+
+
+//return <pre>{ JSON.stringify(data, null, 2)}</pre>
+//}
+import { useEffect, useState } from 'react';
+import { useSupabaseClient } from '@supabase/auth-helpers-react';
+import { createClient } from '@supabase/supabase-js';
+
+
+function listeContacts({ contact }) {
+  if (!contact) {
+    return <p>Loading...</p>;
+  }
+
+  return (
+    <div>
+      <pre>{JSON.stringify(contact, null, 2)}</pre>
+
+    </div>
+  );
+}
+
+
+
+export async function getServerSideProps(context) {
+  const { params } = context;
+  const { id } = params;
+
+  const supabase = createClient(process.env.NEXT_PUBLIC_SUPABASE_URL,
+    process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY)
+
+  const { data } = await supabase.from('contacts').select('*').eq('id', id).single();
+
+  if (!data) {
+    console.error('Contact not found');
+    return {
+      notFound: true,
+    };
+  }
+
+  return {
+    props: {
+      contact: data,
+    },
+  };
+
+}
+
+
+
+
+export default listeContacts;
